573e4d19a208fe45f73b56d89b96fab6c91e3366
KHAN IMRAN <imrankhanshakeel012@gmail.com>
2025-02-26 06:09:44 +0530

    change firebase api's

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAU96sIhibStAz_QBGssTtRyocU_qPuHZ4",
-  authDomain: "notes-app-e546e.firebaseapp.com",
-  projectId: "notes-app-e546e",
-  storageBucket: "notes-app-e546e.firebasestorage.app",
-  messagingSenderId: "209544753604",
-  appId: "1:209544753604:web:764d7862428da943bc7491",
-  dataBaseURL: "https://notes-app-e546e-default-rtdb.firebaseio.com"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
